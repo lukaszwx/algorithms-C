8c6d6266647a5eeb556dd0e1a512e5aa13e47cf4
Lucas Neves <lukas.enginer7@gmail.com>
2026-05-15 16:11:49 -0300

    Update algoritmo_cart_motorista.c
    Signed-off-by: Lucas Neves <lukas.enginer7@gmail.com>

--- algoritmo_cart_motorista.c
+++ algoritmo_cart_motorista.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-int idade;
-char nome[50];
-char exame [20];
+#include <string.h>
+#include <ctype.h>
 
-int main(){
-	printf("Digite seu nome\n");
-	scanf("%s",&nome);
-	printf("Digite a sua idade\n");
-	scanf("%i", &idade);
-	printf("Voce foi aprovado no exame?\n");
-	scanf("%i",&exame);
-	
-if (idade>=18 && idade<=95 && strcmp(exame,"APROVADO") == 0){
-	printf("Voce pode possuir a carteira de motorista, %s:\n",nome);
-} else{
-	printf("Voce nao pode tirar a carteira de motorista, %s:\n",nome);
-	
-}	
+#define TAM_NOME 50
+#define TAM_EXAME 20
+
+void converterParaMaiusculo(char texto[]) {
+    for (int i = 0; texto[i] != '\0'; i++) {
+        texto[i] = toupper((unsigned char) texto[i]);
+    }
 }
 
+int main(void) {
+    char nome[TAM_NOME];
+    char exame[TAM_EXAME];
+    int idade;
+
+    printf("Digite seu nome: ");
+    scanf(" %49[^\n]", nome);
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Voce foi aprovado no exame? Digite APROVADO ou REPROVADO: ");
+    scanf(" %19s", exame);
+
+    converterParaMaiusculo(exame);
+
+    if (idade >= 18 && idade <= 95 && strcmp(exame, "APROVADO") == 0) {
+        printf("\n%s, voce pode possuir carteira de motorista.\n", nome);
+    } else {
+        printf("\n%s, voce nao pode tirar carteira de motorista.\n", nome);
+    }
+
+    return 0;
+}
